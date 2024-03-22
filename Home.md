e35adb9117799c4e3df400b1b0e9fc45c4e41f80
João Carlos <Joao@MacKadu.local>
2024-03-22 00:49:43 -0300

    .

---
+++ Home.md
@@ -0,0 +1,27 @@
+# README.md
+## Who am i?
+- Name: João Zacchello;
+- Languages: Portuguese 🇧🇷, English 🇺🇸;
+- Technologies/frameworks experience:
+  - [NodeJS](https://nodejs.org/en/about) - 2 years;
+  - [React](https://react.dev) - 1 year;
+  - [Typescript](https://www.typescriptlang.org) - 1 year;
+  - [MongoDB](https://www.mongodb.com/atlas/database) - Less than a year;
+  - [Firebase](https://firebase.google.com/?hl=en) - Less than a year;
+  - [Express](https://expressjs.com) - 1 year;
+  - [Python](https://www.python.org) - 2 years;
+  - [Java](https://www.java.com/en/download/help/whatis_java.html) - Less than a year;
+
+<br>
+
+<a href="https://www.github.com/xongs08">
+  <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" alt="Github Badge" width="100px" height="30px" />
+</a>
+
+<a href="https://www.instagram.com/xongs.og">
+  <img src="https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white" alt="Instagram Badge" width="100px" height="30px" />
+</a>
+
+### Projects
+- [Vitemin (vite, but more simple)](https://github.com/zacchellodev/vitemin)
+- [Repo of this site (made with vitemin)](https://github.com/xongs08/new-portfolio)
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { Component } from "react"
+import { useState, useEffect } from "react"
 import Markdown from "react-markdown"
 
-interface State {
-  markdown: string
-}
-
-export default class App extends Component<NonNullable<unknown>, State> {
-  constructor(props: NonNullable<unknown>) {//lint
-    super(props)
-    this.state = { markdown: '' }
-  }
-
-  componentDidMount() {
-    fetch('https://raw.githubusercontent.com/xongs08/xongs08.github.io/main/Markdown.md')
+export default function App() {
+  const [markdown, setMarkdown] = useState('')
+  
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/xongs08/xongs08.github.io/main/Home.md')
       .then(resp => resp.text())
-      .then(data => this.setState({ markdown: data }))
-      .catch(err => console.log(`Não foi possível fazer o request para o Markdown (raw): ${err}`))
-  }
-
-  render() {
-    return <div className="markdown"><Markdown>{this.state.markdown}</Markdown></div>
-  }
+      .then(data => setMarkdown(data))
+      .catch(err => setMarkdown(`# Error: ${err}`))
+  }, [])
+  
+  return (
+    <div className="content" style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      lineHeight: 2,
+      backgroundColor: "#0d1117",
+      paddingTop: "70px",
+      paddingBottom: "70px",
+    }}>
+      <div className="container" style={{
+        color: "#f2f2f2",
+        fontFamily: "JetBrains Mono, monospace",
+        fontSize: 20
+      }}><Markdown>{markdown}</Markdown></div>
+    </div>
+  )
 }
- 
